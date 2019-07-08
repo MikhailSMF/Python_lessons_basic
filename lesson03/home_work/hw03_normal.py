8f13d68eb6a672bc0b74509362997453f8849ce1
Mikhail <mixan116@gmail.com>
2019-07-08 21:31:01 +0400

    hw03 norm done

--- lesson03/home_work/hw03_normal.py
+++ lesson03/home_work/hw03_normal.py
@@ -3,7 +3,16 @@
 # Первыми элементами ряда считать цифры 1 1
 
 def fibonacci(n, m):
-    pass
+    a = 1
+    b = 1
+    lst = [a, b]
+    while len(lst) < m:
+        lst.append(a + b)
+        a, b = b, a + b
+    #print(lst)
+    return lst[n-1:m]
+
+print(fibonacci(3, 5))
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
@@ -12,7 +21,14 @@ def fibonacci(n, m):
 
 
 def sort_to_max(origin_list):
-    pass
+    for i in range(len(origin_list) - 1):
+        for j in range(len(origin_list) - i - 1):
+            if origin_list[j] > origin_list[j + 1]:
+                buff = origin_list[j]
+                origin_list[j] = origin_list[j + 1]
+                origin_list[j + 1] = buff
+    return print(origin_list)
+
 
 sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 
