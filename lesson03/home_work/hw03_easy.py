69e22e3db62fca85227d0e6ff47e6581fa62cfe9
Mikhail <mixan116@gmail.com>
2019-07-08 21:30:32 +0400

    hw03 easy done

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -5,13 +5,25 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
+    """
+    Округление числа без функций
+    :param number: int/float
+    :param ndigits: int
+    :return: int/float
+    """
+    new_number = f'{str(number)[:int(number)+ndigits+1]}'
+    if float(new_number[-1]) >= 6:
+        return float(new_number[:-1]) + float(str(0.)+str('0')*(ndigits-2) + str('1'))
+    else:
+        return new_number
 
 
 print(my_round(2.1234567, 5))
 print(my_round(2.1999967, 5))
 print(my_round(2.9999967, 5))
-
+print(my_round(2.1234567, 3))
+print(my_round(2.1234567, 4))
+print(my_round(2.1234567, 6))
 
 # Задание-2:
 # Дан шестизначный номер билета. Определить, является ли билет счастливым.
@@ -20,7 +32,10 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+    if sum(map(int,list(str(ticket_number)[:-3]))) == sum(map(int,list(str(ticket_number)[-3:]))):
+        return 'Luck'
+    else:
+        return 'No luck'
 
 
 print(lucky_ticket(123006))
